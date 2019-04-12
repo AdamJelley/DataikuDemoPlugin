5e2295db9655517d1ddec379e2b053e32ef4ca92
admin <admin>
2019-04-12 18:54:13 +0100

    Edited files

--- python-connectors/first-plugin_raas/connector.py
+++ python-connectors/first-plugin_raas/connector.py
@@ -25,7 +25,10 @@ class MyConnector(Connector):
         Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
 
         # perform some more initialization
-        self.theparam1 = self.config.get("parameter1", "defaultValue")
+        self.key = self.config["apiKey"]
+        self.nb = int(self.config["nb"])
+        self.max = int(self.config["max"])
+
 
     def get_read_schema(self):
         """
@@ -47,9 +50,11 @@ class MyConnector(Connector):
         Supported types are: string, int, bigint, float, double, date, boolean
         """
 
-        # In this example, we don't specify a schema here, so DSS will infer the schema
-        # from the columns actually returned by the generate_rows method
-        return None
+       return {
+            "columns" : [
+                { "name" : "random", "type" : "int" }
+            ]
+        }
 
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                             partition_id=None, records_limit = -1):
@@ -61,8 +66,15 @@ class MyConnector(Connector):
 
         The dataset schema and partitioning are given for information purpose.
         """
-        for i in xrange(1,10):
-            yield { "first_col" : str(i), "my_string" : "Yes" }
+        req = requests.get("http://raas.dataiku.com/api.php", params = {
+            "apiKey": self.key,
+            "nb":self.nb,
+            "max":self.max
+        })
+
+        array = req.json()
+        for random_number in array:
+            yield { "random"  : random_number}
 
 
     def get_writer(self, dataset_schema=None, dataset_partitioning=None,
